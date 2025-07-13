a0c9f18f2358a10e08c5f77109ba3674771a63d6
Kittisak Porkha <kittisak_pk12@icloud.com>
2025-07-13 13:14:51 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// this is my age
+let myAge = 17;
+// myname in toLowerCase
+const myName = 'froy'.toLowerCase();
+// earlyYear  
+let earlyYear = 2;
+
+earlyYear *= 10.5;
+
+
+let laterYear = myAge - 2;
+
+//  tocal dog year
+laterYear *= 4;
+
+// store dog age
+
+const myAgeinDogYears = earlyYear + laterYear;
+// print to check
+/*console.log(earlyYear);
+console.log(laterYear);
+*/
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeinDogYears} AGE years old in dog years.
+`)
